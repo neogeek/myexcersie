b86723d489dd1e1cca799e7facf987e53cfc7f5f
Scott Doxey <neogeek0101@gmail.com>
2015-11-07 10:29:12 -0500

    Moved JavaScript into separate file.
    Added click event to stop timer.

---
+++ js/app.js
@@ -0,0 +1,86 @@
+(function () {
+
+    'use strict';
+
+    var template = Handlebars.compile($('#sets-template').html()),
+        countdownValue = 3,
+        countdownInterval,
+        timerValue = 0,
+        timerInterval;
+
+    $.getJSON('/data/sets.json').done(function (data) {
+
+        $('.sets').html(template({ sets: data }));
+
+    });
+
+    FastClick.attach(document.body);
+
+    function countdown() {
+
+        if (countdownValue) {
+
+            $('.set-timer-wrapper .number').text(countdownValue--);
+
+        } else {
+
+            clearInterval(countdownInterval);
+
+            timerValue = 0;
+            timer();
+            timerInterval = setInterval(timer, 1000);
+
+        }
+
+    }
+
+    function timer() {
+
+        if (timerValue <= parseInt($('.sets .set .item.active').data('seconds'), 10)) {
+
+            $('.set-timer-wrapper .number').text(timerValue++);
+
+        } else {
+
+            clearInterval(timerInterval);
+
+            $('.sets .set .item.active').addClass('done').removeClass('active');
+
+            $('.set-timer-wrapper').addClass('hidden');
+
+        }
+
+    }
+
+    $('.sets').on('click', '.set li', function () {
+
+        var $this = $(this);
+
+        if (parseInt($this.data('seconds'), 10) > 0 && !$this.hasClass('done')) {
+
+            $this.addClass('active');
+
+            $('.set-timer-wrapper').removeClass('hidden');
+
+            countdownValue = 3;
+            countdown();
+            countdownInterval = setInterval(countdown, 1000);
+
+        } else {
+
+            $this.toggleClass('done');
+
+        }
+
+    });
+
+    $('.set-timer-wrapper').on('click', function () {
+
+        $(this).addClass('hidden');
+
+        clearInterval(countdownInterval);
+        clearInterval(timerInterval);
+
+    });
+
+}());
